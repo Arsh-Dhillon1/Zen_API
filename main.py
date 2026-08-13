6a10919600d4d9fdb2dbc2448b0008db77af6e80
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-08-13 14:00:47 +0530

    fix: untrack pycache and respect gitignore rules

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class Journal(BaseModel):
+    content:str
 
 @app.get("/")
 def check_status():
     return {"message":"Zen Journal API is running"}
 
+@app.post("/journals")
+def journals(journal:Journal):
+    return {f"Content Successfully sent to the server! {journal.content}"}
+    
