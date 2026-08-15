150181fb9795ef973b15744681a5b8110cce10fd
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-08-15 23:16:41 +0530

    implement automatic validation of mood entry using enum and made mood attribute optional

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
 journal_entries = []
 next_id = 1
 
+class Mood(str, Enum):
+    HAPPY = "happy"
+    CALM = "calm"
+    SAD = "sad"
+    ANGRY = "angry"
+    ANXIOUS = "anxious"
+    GRATEFUL = "grateful"
+    EXCITED = "excited"
+    NEUTRAL = "neutral"
+
 class JournalCreate(BaseModel):
     content:str
+    mood:Mood | None = None
 
 @app.get("/journals/{journal_id}")
 def show_entry(journal_id:int):
@@ -23,10 +35,14 @@ def show_entries():
 @app.post("/journals")
 def create_journal(journal:JournalCreate):
     global next_id
+    
     new_entry = {
-        "id":next_id,
-        "content":journal.content
+    "id": next_id,
+    "content": journal.content
     }
+    if journal.mood:
+        new_entry["mood"] = journal.mood
+
     journal_entries.append(new_entry)
     next_id+=1
     return new_entry
